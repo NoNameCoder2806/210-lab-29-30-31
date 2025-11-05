17c9b38eb9868224e44b245b973278d3bf48014e
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-11-05 12:52:36 -0800

    Still reading the data huhuuu

--- main.cpp
+++ main.cpp
@@ -17,6 +17,9 @@ using namespace std;
 // Constants
 const int MIN = 3;
 const int MAX = 5;
+const int LAND_INDEX = 0;
+const int WATER_INDEX = 1;
+const int AIR_INDEX = 2;
 const string DATA_FILE = "data.txt";
 
 // Function prototypes
@@ -46,6 +49,16 @@ void readData(map<string, array<list<Creature>, 3>> &allCreatures, string data_p
     ifstream fin;
     fin.open(data_path);
 
+    // Check for file open failure
+    if (!fin)
+    {
+        // Display an error message
+        cerr << "Error opening file" << endl;
+        
+        // Exit the function
+        return;
+    }
+
     // Create a string to store the line's data
     string line;
 
@@ -68,7 +81,29 @@ void readData(map<string, array<list<Creature>, 3>> &allCreatures, string data_p
         if (isEra(line))
         {
             // If this line is the era, we store the key
+            key = line;
+
+            // We clear all the lists as well
+            landCreatures.clear();
+            waterCreatures.clear();
+            airCreatures.clear();
+
+            // Skip to the next line
+            continue;
+        }
+
+        // Check for the Creatures type
+        if (line.find("LAND: ") == 0)
+        {
             
+        }
+        else if (line.find("WATER: ") == 0)
+        {
+
+        }
+        else if (line.find("AIR: " == 0))
+        {
+
         }
     }
 
